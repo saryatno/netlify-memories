26ac5d84bfa62d1509695fe29d407e578474eaf3
saryatno <saryatno@gmail.com>
2022-12-31 20:36:22 +0700

    last changes

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/posts';
+const url = 'https://tense-fly-sweatsuit.cyclic.app/posts';
 //const newPost = axios.request()
 
 export const fetchPosts = () => axios.get(url);
